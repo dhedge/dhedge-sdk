434f66b2921dbc9904373aae8b0495c6540181fd
Vidor Gencel <vidorge@gmail.com>
2020-10-15 03:04:17 +0200

    Env file

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+
+if (!envFound) {
+	throw new Error("Couldn't find .env file.");
+}
+
+module.exports = {
+	mnemonic: process.env.MNEMONIC || '',
+	accountId: process.env.ACCOUNT_ID || '0',
+	provider: process.env.PROVIDER || 'http://localhost:8545',
+	factoryAddress: process.env.FACTORY_ADDRESS || '0x0',
+};
