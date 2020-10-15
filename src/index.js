3798f7168d8654119e7bb67503c1de128af901c5
Vidor Gencel <vidorge@gmail.com>
2020-10-15 03:06:57 +0200

    Export classes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Pool = require('./pool')
+const Factory = require('./factory')
+const Token = require('./token')
+
+module.exports = {
+    Pool: Pool,
+    Factory: Factory,
+    Token: Token,
+}
