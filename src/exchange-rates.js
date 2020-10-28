894d79c294126bc4ba1efdd8e610b5a449acbf80
Vidor Gencel <vidorge@gmail.com>
2020-10-28 07:35:44 +0100

    rateForCurrency and getEffectiveValue for exchange rates

--- src/exchange-rates.js
+++ src/exchange-rates.js
@@ -1,3 +1,4 @@
+const { Contract, Wallet, ethers, BigNumber } = require('ethers')
 const EXCHANGE_RATES_ABI = require('./abi/exchange-rates')
 
 class ExchangeRates {
@@ -13,5 +14,26 @@ class ExchangeRates {
         this.address = exchangeRatesAddress
     }
 
+    async rateForCurrency(key) {
+        key = ethers.utils.formatBytes32String(key)
+
+        return this.contract.rateForCurrency(key)
+    }
+
+    async getEffectiveValue(source, amount, destination) {
+        source = ethers.utils.formatBytes32String(source)
+        destination = ethers.utils.formatBytes32String(destination)
+
+        if (!BigNumber.isBigNumber(amount) && typeof amount !== 'string') {
+            throw new Error(
+                'Please pass numbers as strings or BigNumber objects to avoid precision errors.'
+            )
+        }
+
+        amount = ethers.BigNumber.from(amount)
+
+        return this.contract.effectiveValue(source, amount, destination)
+    }
+}
 
 module.exports = ExchangeRates
