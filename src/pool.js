76e70184e2948f6eb2f5f79200414395260b98f3
Vidor Gencel <vidorge@gmail.com>
2020-11-04 01:37:58 +0100

    Pool jsdoc

--- src/pool.js
+++ src/pool.js
@@ -6,6 +6,10 @@ const TOKEN_ABI = require('./abi/token')
 const Token = require('./token')
 
 class Pool {
+    /**
+     * @param {Signer} signer
+     * @param {string} poolAddress
+     */
     constructor(signer, poolAddress) {
         this.signer = signer
 
@@ -15,32 +19,68 @@ class Pool {
         this.address = poolAddress
     }
 
+    /**
+     * Returns the pool token instance.
+     * 
+     * @returns {Token}
+     */
     token() {
-        return this.token
+        return new Token(this.signer, this.address)
     }
 
+    /**
+     * Returns the pool address.
+     * 
+     * @returns {string}
+     */
     getAddress() {
         return this.address
     }
 
     // Read - Managed
 
+    /**
+     * Returns the pool manager address.
+     * 
+     * @returns {Promise<string>}
+     */
     async getManager() {
         return this.contract.manager()
     }
 
+    /**
+     * Returns the pool manager name.
+     * 
+     * @returns {Promise<string>}
+     */
     async getManagerName() {
         return this.contract.managerName()
     }
 
+    /**
+     * Returns if the supplied address is a pool member.
+     * 
+     * @param {string} address
+     * @returns {Promise<boolean>}
+     */
     async isMember(address) {
         return this.contract.isMemberAllowed(address)
     }
 
+    /**
+     * Returns all pool member addresses.
+     * @returns {Promise<string[]>}
+     */
     async getMembers() {
         return this.contract.getMembers()
     }
 
+    /**
+     * Returns the member count.
+     * 
+     * @param raw
+     * @returns {Promise<number|BigNumber>}
+     */
     async getMemberCount(raw = false) {
         let result = await this.contract.numberOfMembers()
 
@@ -51,6 +91,13 @@ class Pool {
 
     // Write - Managed
 
+    /**
+     * Transfers the manager role to the supplied address with and changes the manager name.
+     * 
+     * @param {string} address
+     * @param {string} name
+     * @returns {Promise<void>}
+     */
     async changeManager(address, name) {
         let receipt = await this.contract.changeManager(address, name)
 
@@ -63,36 +110,76 @@ class Pool {
         if (!changeEvent) throw new Error('Manager not updated')
     }
 
+    /**
+     * Batch add members.
+     * 
+     * @param {string[]} members
+     * @returns {Promise<void>}
+     */
     async addMembers(members) {
         let receipt = await this.contract.addMembers(members)
 
         receipt = await receipt.wait(1)
     }
 
+    /**
+     * Batch remove members.
+     * 
+     * @param {string[]} members
+     * @returns {Promise<void>}
+     */
     async removeMembers(members) {
         let receipt = await this.contract.removeMembers(members)
 
         receipt = await receipt.wait(1)
     }
 
+    /**
+     * Add a member.
+     * 
+     * @param {string} member
+     * @returns {Promise<void>}
+     */
     async addMember(member) {
         this.addMembers([member])
     }
 
+    /**
+     * Remove a member.
+     * 
+     * @param {string} member
+     * @returns {Promise<void>}
+     */
     async removeMember(member) {
         this.removeMembers([member])
     }
 
     // Read - Pool
 
+    /**
+     * Returns if a pool is private
+     * 
+     * @returns {Promise<boolean>}
+     */
     async isPrivate() {
         return this.contract.privatePool()
     }
 
+    /**
+     * Returns the address of the pool creator.
+     * 
+     * @returns {Promise<string>}
+     */
     async getCreator() {
         return this.contract.creator()
     }
 
+    /**
+     * Returns the pool creation time.
+     *
+     * @param {boolean} raw
+     * @returns {Promise<number|BigNumber>}
+     */
     async getCreationTime(raw = false) {
         let result = await this.contract.creationTime()
 
@@ -101,20 +188,42 @@ class Pool {
         return result.toNumber()
     }
 
+    /**
+     * Returns the factory address.
+     * 
+     * @returns {Promise<string>}
+     */
     async getFactory() {
         return this.contract.factory()
     }
 
+    /**
+     * Returns all pool assets.
+     * 
+     * @returns {Promise<string[]>}
+     */
     async getAssets() {
         return (await this.contract.getSupportedAssets()).map(item =>
             ethers.utils.parseBytes32String(item)
         )
     }
 
+    /**
+     * Returns the last token price at which the manager fee was minted.
+     * 
+     * @returns {Promise<BigNumber>}
+     */
     async getTokenPriceAtLastFeeMint() {
         return this.contract.tokenPriceAtLastFeeMint()
     }
 
+    /**
+     * Returns the timestamp of the last deposit of the supplied address.
+     * 
+     * @param {string} address
+     * @param {boolean} raw
+     * @returns {Promise<number|BigNumber>}
+     */
     async getLastDepositByAddress(address, raw = false) {
         let result = await this.contract.lastDeposit(address)
 
@@ -123,6 +232,12 @@ class Pool {
         return result.toNumber()
     }
 
+    /**
+     * Returns the manager fee.
+     * 
+     * @param {boolean} raw
+     * @returns {Promise<number|BigNumber>}
+     */
     async getManagerFee(raw = false) {
         let result = await this.contract.getManagerFee()
 
@@ -134,7 +249,13 @@ class Pool {
         return result
     }
 
-    async getManagerFee(raw = false) {
+    /**
+     * Returns the exit fee.
+     * 
+     * @param {boolean} raw
+     * @returns {Promise<number|BigNumber>}
+     */
+    async getExitFee(raw = false) {
         let result = await this.contract.getExitFee()
 
         if (raw) return result
@@ -145,6 +266,12 @@ class Pool {
         return result
     }
 
+    /**
+     * Returns the asset count.
+     * 
+     * @param {boolean} raw
+     * @returns {Promise<number|BigNumber>}
+     */
     async getAssetCount(raw = false) {
         let result = await this.contract.numberOfSupportedAssets()
 
@@ -153,12 +280,25 @@ class Pool {
         return result.toNumber()
     }
 
+    /**
+     * Returns true if the asset is supported.
+     * 
+     * @param {string} key
+     * @param {boolean} convert
+     * @returns {Promise<boolean>}
+     */
     async isAssetSupported(key, convert = false) {
         if (convert) key = ethers.utils.formatBytes32String(key)
 
         return this.contract.isAssetSupported(key)
     }
 
+    /**
+     * Returns the Token instance of the supplied asset.
+     * 
+     * @param {string} key
+     * @returns {Promise<Token>}
+     */
     async getAsset(key) {
         key = ethers.utils.formatBytes32String(key)
 
@@ -170,10 +310,21 @@ class Pool {
         return new Token(this.signer, proxy)
     }
 
+    /**
+     * Returns the pool value.
+     * 
+     * @returns {Promise<BigNumber>}
+     */
     async getPoolValue() {
         return this.contract.totalFundValue()
     }
 
+    /**
+     * Returns the asset value of the given asset.
+     * 
+     * @param {string} key
+     * @returns {Promise<BigNumber>}
+     */
     async assetValue(key) {
         key = ethers.utils.formatBytes32String(key)
 
@@ -183,6 +334,11 @@ class Pool {
         return this.contract.assetValue(key)
     }
 
+    /**
+     * Returns the fund summary
+     * 
+     * @returns {Promise<{exitFee: number, managerFee: number, private: boolean, managerAddress: string, creationTime: number, totalSupply: BigNumber, name: string, managerName: string, totalPoolValue: BigNumber}>}
+     */
     async getSummary() {
         let summary = await this.contract.getFundSummary()
 
@@ -199,6 +355,11 @@ class Pool {
         }
     }
 
+    /**
+     * Returns the fund composition.
+     * 
+     * @returns {Promise<any>}
+     */
     async getComposition() {
         let result = await this.contract.getFundComposition()
 
@@ -214,6 +375,11 @@ class Pool {
         return composition
     }
 
+    /**
+     * Returns waiting periods for all assets.
+     * 
+     * @returns {Promise<Object>}
+     */
     async getWaitingPeriods() {
         let result = await this.contract.getWaitingPeriods()
 
@@ -228,6 +394,11 @@ class Pool {
         return periods
     }
 
+    /**
+     * Get a map of suspended assets.
+     * 
+     * @returns {Promise<Object>}
+     */
     async getSuspendedAssets() {
         let result = await this.contract.getSuspendedAssets()
 
@@ -242,6 +413,12 @@ class Pool {
 
     // Pool - Write
 
+    /**
+     * Add a supported asset.
+     * 
+     * @param {string} key
+     * @returns {Promise<void>}
+     */
     async addAsset(key) {
         key = ethers.utils.formatBytes32String(key)
 
@@ -252,6 +429,12 @@ class Pool {
         return this.contract.addToSupportedAssets(key)
     }
 
+    /**
+     * Remove a supported asset.
+     * 
+     * @param {string} key
+     * @returns {Promise<void>}
+     */
     async removeAsset(key) {
         key = ethers.utils.formatBytes32String(key)
 
@@ -262,6 +445,12 @@ class Pool {
         return this.contract.removeFromSupportedAssets(key)
     }
 
+    /**
+     * Deposit
+     * 
+     * @param {string|BigNumber} amount
+     * @returns {Promise<void>}
+     */
     async deposit(amount) {
         if (!BigNumber.isBigNumber(amount) && typeof amount !== 'string') {
             throw new Error(
@@ -276,6 +465,13 @@ class Pool {
         await tx.wait(1)
     }
 
+    /**
+     * Withdraw
+     * 
+     * @param {string|BigNumber} amount
+     * @param {boolean} forfeit
+     * @returns {Promise<void>}
+     */
     async withdraw(amount, forfeit = false) {
         if (!BigNumber.isBigNumber(amount) && typeof amount !== 'string') {
             throw new Error(
@@ -294,6 +490,14 @@ class Pool {
         await tx.wait(1)
     }
 
+    /**
+     * Exchange
+     * 
+     * @param {string} sourceKey
+     * @param {string|BigNumber} sourceAmount
+     * @param {string} destinationKey
+     * @returns {Promise<void>}
+     */
     async exchange(sourceKey, sourceAmount, destinationKey) {
         sourceKey = ethers.utils.formatBytes32String(sourceKey)
         destinationKey = ethers.utils.formatBytes32String(destinationKey)
